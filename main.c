a61b66b57f526dbcd97af1204f149d16c26a36ae
ImadElMaftouhi <imad.elmaftouhi@etu.uae.ac.ma>
2024-02-15 19:22:32 +0100

    added AfficherMatriceAdjacence()

--- main.c
+++ main.c
@@ -31,3 +31,60 @@ struct pathData{
 };
 
 
+/*------------------ D E C L A R A T I O N S ---------------*/
+
+int **AfficherMatriceAdjacence(struct Graphe *G);
+
+
+
+
+
+
+/*---------------------- F O N C T I O N S-------------------*/
+
+
+
+/**
+ * Une fonction qui permet l'affichage de notre matrice
+ * @param G : Pointeur de type << Graphe* >>
+ */
+int **AfficherMatriceAdjacence(struct Graphe *G) {
+    if ( G == NULL){
+        printf("\nThe G graph is NULL\n");
+        return NULL;
+    }
+    struct voisinage *temp = NULL;
+
+    int ** matrice = (int**)malloc( G->sommets * sizeof(int*) );
+    
+    for (int i = 0 ; i < G->sommets ; i++){
+        matrice[i] = (int*)malloc(sizeof(int) * G->sommets);
+    }
+
+
+    int poid = 0, width = 7;
+    printf("\nMatrice d'adjacence de graphe :\n");
+    for (int i = 0; i < G->sommets; i++) {
+        for (int j = 0; j < G->sommets; j++) {
+            temp = G->list[i];
+            poid = 0;
+
+            while (temp != NULL) {
+                if (temp->destination == j) {
+                    poid = temp->poid;
+                    break;
+                }
+                temp = temp->next;
+            }  
+
+            matrice[i][j] = poid;
+            printf("%-*d",width , poid);
+            if (j < G->sommets - 1) {
+                printf(" ");
+            }
+
+        }
+        printf("\n");
+    }
+    return matrice;
+}
